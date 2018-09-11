a2b0c39575901bdce0391f01692075112f447be6
Ayan Pal <ayangitrepo@gmail.com>
2018-09-12 02:08:20 +0530

    java doc added

--- ArrayOperations/src/com/hacos/array/rotation/jugglingalgo/ReversalAlgo.java
+++ ArrayOperations/src/com/hacos/array/rotation/jugglingalgo/ReversalAlgo.java
@@ -1,10 +1,21 @@
 package com.hacos.array.rotation.jugglingalgo;
 
-/*Idea of this algorithm is to first divide the main array in two parts, one from 0 to number of rotations to be performed and other is from number of rotations to the last array element.
-now reverse both the arrays, them merge them as they are and reverse the whole merged array again to find the final product.*/
 
+/* Idea of this algorithm is to first divide the main array in two parts, one from 0 to number of rotations to be performed and 
+ * other is from number of rotations to the last array element. 
+ * now reverse both the arrays, them merge them as they are and reverse the whole merged array again to find the final product.
+ * */
+
+/**
+ * The Class ReversalAlgo.
+ */
 public class ReversalAlgo {
 
+	/**
+	 * The main method.
+	 *
+	 * @param args the arguments
+	 */
 	public static void main(String[] args) {
 		int array[] = new int[] {1,2,3,4,5,6,7,8,9,10,11,12};
 		int rotation = 3;
@@ -18,6 +29,13 @@ public class ReversalAlgo {
 
 	}
 
+	/**
+	 * Rotate with reverse algo.
+	 *
+	 * @param array the array
+	 * @param rotation the rotation
+	 * @return the int[]
+	 */
 	private static int[] rotateWithReverseAlgo(int[] array, int rotation) {
 		reverse(array,0,rotation-1);
 		reverse(array,rotation,array.length-1);
@@ -26,6 +44,13 @@ public class ReversalAlgo {
 		return array;
 	}
 
+	/**
+	 * Reverse.
+	 *
+	 * @param array the array
+	 * @param start the start
+	 * @param end the end
+	 */
 	//reversing array elements part by part
 	private static void reverse(int[] array, int start, int end) {
 		int count = 0;
